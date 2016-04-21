232b85c2dfebf631629923faadbd32465937fa14
Marco <marco@polocase1@gmail.com>
2016-04-21 13:38:30 -0400

    correjido

--- SocialN/rSocial/urls.py
+++ SocialN/rSocial/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
-    url(r'^registro/$', views.registro, name='registro'),
+    url(r'^registro$', views.registro, name='registro'),
     url(r'^Inicio/$', views.Inicio, name='Inicio'),
     url(r'^salir/(.+)$', views.salir, name='salir'),
     url(r'^Perfil/(.+)$', views.Perfil, name='Perfil'),
